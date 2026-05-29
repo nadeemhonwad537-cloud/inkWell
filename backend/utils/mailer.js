096b3d626d60b243503bf14a2d0c3c7037107394
Nadeem <nadeemhonwad537@gmail.com>
2026-05-30 02:07:15 +0530

    fix smtp port 587 for render

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,10 +1,15 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
